0fdb480760804c24a5b2a00510f3a6fe8ac9f7ce
Cinthia M. Orona <devrider2021@gmail.com>
2023-04-24 19:10:06 -0300

    .

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 function Footer() {
     return (
         <footer class="page-footer bg-dark">
-            <div class="footer-copyright text-center text-secondary py-3">© CaC React #22803 2022 Copyright: Cinthia Orona. Made with
+            <div class="footer-copyright text-center text-secondary py-3">© CaC React #23301 2023 Copyright: Cinthia Orona. Made with
                 <a> <i class="fas fa-heart"></i> </a>
             </div>
         </footer>
